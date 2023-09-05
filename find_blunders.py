14dcbbec98b2e44971954a08cde23520085ca40b
LMDN (Lasse Hjorth Madsen) <lmdn@novonordisk.com>
2023-09-05 23:08:44 +0200

    first draft finished

--- find_blunders.py
+++ find_blunders.py
@@ -13,16 +13,39 @@
 # > from find_blunders import *                                              # Note: GNUBG should be restarted to load any changes 
 
 import gnubg
+import os
+import glob
 
-def get_files():
-    files = ['scripts\match1484923.txt', 'scripts\match1486073.txt']
+def get_files(path="c:\\Users\\LMDN\\AppData\\Local\\gnubg\\scripts\\matches", extension="txt"):
+    search_pattern = os.path.join(path, f"*.{extension}")
+    files = glob.glob(search_pattern)
     return files
 
-def convert_files(files):
-    file = files[1] # Replace with loop
-    gnubg.command('import auto ' + file)
-    file = file[:-3] + "sgf"
-    gnubg.command('save match "' + file)
+def analyze_files(files, out_dir = "c:\\Users\\LMDN\\AppData\\Local\\gnubg\\scripts\\output", ply=3):
+    # Make sure the directory exists, and if not, create it
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+        
+    # Parameters for analysis
+    gnubg.command('set analysis cube on')
+    gnubg.command('set analysis moves on')
+    gnubg.command('set analysis chequerplay type evaluation')
+    gnubg.command('set analysis chequerplay evaluation prune on')
+    gnubg.command('set analysis chequerplay evaluation cubeful on')
+    gnubg.command(f'set analysis chequerplay evaluation plies {ply}')
+    
+    for file in files:
+        # Import the text match file
+        gnubg.command(f'import auto {file}')
 
+        # Analyze match
+        gnubg.command('analyze match')
+        
+        # Create the name for the new .sgf and .txt files based on the original filename
+        file_name = os.path.basename(file)  # Extract just the filename without path
+        sgf_file = os.path.join(out_dir, file_name[:-4] + "_analyzed.sgf")
+        txt_file = os.path.join(out_dir, file_name[:-4] + "_analyzed.txt")
 
-
+        # Export the match to .sgf and .txt format
+        gnubg.command(f'save match "{sgf_file}"')
+        gnubg.command(f'export match text "{txt_file}"')
