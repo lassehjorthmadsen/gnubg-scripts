962a2d2275f4baad368e66a3aafcf25e735766c6
Lasse Hjorth Madsen <lassehjorthmadsen@gmail.com>
2023-09-07 14:59:04 +0200

    more tweaks

--- find_blunders.py
+++ find_blunders.py
@@ -16,12 +16,12 @@ import gnubg
 import os
 import glob
 
-def get_files(path="c:\\Users\\LMDN\\AppData\\Local\\gnubg\\scripts\\matches", extension="txt"):
+def get_files(path="c:\\Users\\lasse\\Dropbox\\Backgammon\\Matches\\Galaxy matches\\raw\\", extension="txt"):
     search_pattern = os.path.join(path, f"*.{extension}")
     files = glob.glob(search_pattern)
     return files
 
-def analyze_files(files, out_dir = "c:\\Users\\LMDN\\AppData\\Local\\gnubg\\scripts\\output", ply=3):
+def analyze_files(files, out_dir = "c:\\Users\\lasse\\Dropbox\\Backgammon\\Matches\\Galaxy matches\\analyzed\\", ply=3):
     # Make sure the directory exists, and if not, create it
     if not os.path.exists(out_dir):
         os.makedirs(out_dir)
@@ -29,6 +29,7 @@ def analyze_files(files, out_dir = "c:\\Users\\LMDN\\AppData\\Local\\gnubg\\scri
     # Parameters for analysis
     gnubg.command('set analysis cube on')
     gnubg.command('set analysis moves on')
+    gnubg.command('set analysis luck off')
     gnubg.command('set analysis chequerplay type evaluation')
     gnubg.command('set analysis chequerplay evaluation prune on')
     gnubg.command('set analysis chequerplay evaluation cubeful on')
@@ -36,7 +37,7 @@ def analyze_files(files, out_dir = "c:\\Users\\LMDN\\AppData\\Local\\gnubg\\scri
     
     for file in files:
         # Import the text match file
-        gnubg.command(f'import auto {file}')
+        gnubg.command(f'import auto "{file}"')
 
         # Analyze match
         gnubg.command('analyze match')
