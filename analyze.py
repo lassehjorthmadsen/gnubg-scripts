004188cbf9d3f6a375e39a08fa30172bc539be1e
Lasse Hjorth Madsen <lassehjorthmadsen@gmail.com>
2023-09-16 15:06:36 +0200

    changed default dirs, skip existing files, invert board

--- analyze.py
+++ analyze.py
@@ -11,17 +11,25 @@
 
 # For this script:
 # > from find_blunders import *                                              # Note: GNUBG should be restarted to load any changes 
-
+# Example of a command line 
+# import auto "c:/Users/lasse/Dropbox/Backgammon/Matches/Galaxy matches/raw/match972338.txt"
+# import auto "c:\Users\lasse\Dropbox\Backgammon\Matches\Galaxy matches\raw\match972338.txt"
 import gnubg
 import os
 import glob
 
-def get_files(path="c:\\Users\\lasse\\Dropbox\\Backgammon\\Matches\\Galaxy matches\\raw\\", extension="txt"):
+def get_files(path="c:\\Users\\lasse\\R-projects\\backgammon\\data-raw\\galaxy-matches\\raw\\", extension="txt"):
     search_pattern = os.path.join(path, f"*.{extension}")
     files = glob.glob(search_pattern)
     return files
 
-def analyze_files(files, out_dir = "c:\\Users\\lasse\\Dropbox\\Backgammon\\Matches\\Galaxy matches\\analyzed\\", ply=3, sgf=False):
+def maybe_swap_players():
+    match_info = gnubg.match()['match-info']
+    o_player_name = match_info['O']['name']
+    if o_player_name != 'lasse':
+        gnubg.command('swap players')
+
+def analyze_files(files, out_dir = "c:\\Users\\lasse\\R-projects\\backgammon\\data-raw\\galaxy-matches\\analyzed\\", ply=3, sgf=False):
     # Make sure the directory exists, and if not, create it
     if not os.path.exists(out_dir):
         os.makedirs(out_dir)
@@ -42,16 +50,23 @@ def analyze_files(files, out_dir = "c:\\Users\\lasse\\Dropbox\\Backgammon\\Match
     gnubg.command(f'set analysis cubedecision evaluation plies {ply}')
     
     for file in files:
+        # Create the name for the new .sgf and .txt files based on the original filename
+        file_name = os.path.basename(file)  # Extract just the filename without path
+        sgf_file = os.path.join(out_dir, file_name[:-4] + ".sgf")
+        txt_file = os.path.join(out_dir, file_name[:-4] + ".txt")
+        
+        # Check if the file has already been analyzed
+        if os.path.exists(txt_file):
+            continue
+
         # Import the text match file
         gnubg.command(f'import auto "{file}"')
 
+        # Check and maybe swap players
+        maybe_swap_players()
+
         # Analyze match
         gnubg.command('analyze match')
-        
-        # Create the name for the new .sgf and .txt files based on the original filename
-        file_name = os.path.basename(file)  # Extract just the filename without path
-        sgf_file = os.path.join(out_dir, file_name[:-4] + "_analyzed.sgf")
-        txt_file = os.path.join(out_dir, file_name[:-4] + "_analyzed.txt")
 
         # Export the match to .sgf and .txt format
         if sgf:
